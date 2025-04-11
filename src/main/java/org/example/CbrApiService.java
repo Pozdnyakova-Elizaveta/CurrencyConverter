55e524be1bb66a3ebd9271cafeab118365dd1cf1
Elizaweta <liza_pozdn03@mail.ru>
2025-04-11 12:28:11 +0700

    Added getExchangeRate() trivial implementation

--- src/main/java/org/example/CbrApiService.java
+++ src/main/java/org/example/CbrApiService.java
@@ -16,4 +16,10 @@ public class CbrApiService {
         else if (date.equals(LocalDate.of(2025, 4, 1))) return XmlResponse.XML_RESPONSE_APRIL_1;
         throw new IllegalArgumentException("getXmlResponseRateByDate() works only April 5, 4, 3, 2, 1");
     }
+
+    public double getExchangeRate(String currency, LocalDate date) {
+        //TODO реализовать с использованием getXmlResponseRateByDate
+        if (currency.equals("USD") && date.equals(LocalDate.of(2025, 4, 5))) return 84.2774;
+        throw new IllegalArgumentException("getXmlResponseRateByDate() works only with parameters (USD, 05.04.2025)");
+    }
 }
