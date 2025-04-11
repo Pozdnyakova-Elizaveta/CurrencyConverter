605cb4ab04631cd26ce056596dc61f18bf10e0ff
Elizaweta <liza_pozdn03@mail.ru>
2025-04-11 11:22:48 +0700

    Refactoring getXmlResponseRateByDate() for pass the test April 4, 5

--- src/main/java/org/example/CbrApiService.java
+++ src/main/java/org/example/CbrApiService.java
@@ -10,6 +10,7 @@ public class CbrApiService {
     public String getXmlResponseRateByDate(LocalDate date) {
         //TODO подключить api
         if (date.equals(LocalDate.of(2025, 4, 5))) return XmlResponse.XML_RESPONSE_APRIL_5;
-        throw new IllegalArgumentException("getXmlResponseRateByDate() works only April 5");
+        if (date.equals(LocalDate.of(2025, 4, 4))) return XmlResponse.XML_RESPONSE_APRIL_4;
+        throw new IllegalArgumentException("getXmlResponseRateByDate() works only April 5, 4");
     }
 }
