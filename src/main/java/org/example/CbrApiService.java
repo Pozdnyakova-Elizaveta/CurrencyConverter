f405f0db873093f232ee507b849d44692eb5dbd1
Elizaweta <liza_pozdn03@mail.ru>
2025-04-11 11:12:39 +0700

    Added getXmlResponseRateByDate() trivial implementation

--- src/main/java/org/example/CbrApiService.java
+++ src/main/java/org/example/CbrApiService.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class CbrApiService {
     public CbrApiService(){
 
+    }
+
+    public String getXmlResponseRateByDate(LocalDate date) {
+        //TODO подключить api
+        if (date.equals(LocalDate.of(2025, 4, 5))) return XmlResponse.XML_RESPONSE_APRIL_5;
+        throw new IllegalArgumentException("getXmlResponseRateByDate() works only April 5");
     }
 }
