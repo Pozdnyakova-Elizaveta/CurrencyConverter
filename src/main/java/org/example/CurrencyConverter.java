2814c1d80d6f683347064a6b255a3e4042780223
Elizaweta <liza_pozdn03@mail.ru>
2025-04-13 16:40:54 +0700

    Refactoring convert() for pass the test (300, RUB, KZT, 03.04.2025), (200, RUB, USD, 05.04.2025) and amount less than zero

--- src/main/java/org/example/CurrencyConverter.java
+++ src/main/java/org/example/CurrencyConverter.java
@@ -13,6 +13,9 @@ public class CurrencyConverter {
         else if (amount==200 && fromCurrency.equals("RUB") && toCurrency.equals("USD") &&
                 date.equals(LocalDate.of(2025, 4, 5)))
             return 2.373115449693512;
+        else if (amount==300 && fromCurrency.equals("RUB") && toCurrency.equals("KZT") &&
+                date.equals(LocalDate.of(2025, 4, 3)))
+            return 1785.2254739773634;
         throw new IllegalArgumentException("convert() works only with parameters (200, RUB, USD, 05.04.2025) and amount less than zero");
     }
 }
