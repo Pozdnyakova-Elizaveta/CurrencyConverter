b2b77a91da0bc8c5724956035300433b4bdbe60e
Elizaweta <liza_pozdn03@mail.ru>
2025-04-13 15:51:44 +0700

    Added test convert() for amount less than zero

--- src/test/java/CurrencyConverterTest.java
+++ src/test/java/CurrencyConverterTest.java
@@ -2,10 +2,21 @@ import org.example.CurrencyConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class CurrencyConverterTest {
     @Test
     void currencyConverterClassCreationTest() {
         CurrencyConverter converter = new CurrencyConverter();
         Assertions.assertNotNull(converter);
+    }
+    @Test
+    void convertingAmountLessThanZeroTest() {
+        CurrencyConverter converter = new CurrencyConverter();
+        Exception exception = Assertions.assertThrows(Exception.class, () ->
+                converter.convert(-1, "RUB", "USD", LocalDate.of(2025,4,4)));
+        String expectedMessage = "Нельзя конвертировать сумму меньше нуля";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
     }
 }
