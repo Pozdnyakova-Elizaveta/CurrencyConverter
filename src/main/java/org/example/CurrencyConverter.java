6e31ffe3eeb1e78b55db5adfe64aa9ea73787450
Elizaweta <liza_pozdn03@mail.ru>
2025-04-13 16:09:29 +0700

    Added convert() trivial implementation

--- src/main/java/org/example/CurrencyConverter.java
+++ src/main/java/org/example/CurrencyConverter.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class CurrencyConverter {
     public CurrencyConverter(){
 
+    }
+
+    public void convert(double amount, String fromCurrency, String toCurrency, LocalDate date) throws Exception {
+        if (amount<0) throw new Exception("Нельзя конвертировать сумму меньше нуля");
+        throw new IllegalArgumentException("convert() works only with amount less than zero");
     }
 }
